1b5bed45bb490a83cc55d82ba19f7b4fdd9b924d
rzl <3134467344@qq.com>
2021-08-03 16:11:57 +0800

    first commit

---
+++ src/main/java/com/rzl/hello.java
@@ -0,0 +1,12 @@
+package com.rzl;
+
+/**
+ * @auther rzl
+ * @create 2021-08-03 16:01
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
